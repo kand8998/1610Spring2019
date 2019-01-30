73c8d2699fbff0f401b855f95659582a5ddafe8b
Kaitlyn <kand8998@gmail.com>
2019-01-30 13:17:06 -0700

    Physics, Particles, Events Added
    Unity Project

---
+++ FirstGame1610/Assets/MushroomEvent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class MushroomEvent : MonoBehaviour
+{
+	public UnityEvent Event;
+
+	private void OnCollisionEnter2D()
+	{
+		Event.Invoke();
+	}
+}
