eb6fc35b538931ed63524c07e527661ced78c609
Kaitlyn <kand8998@gmail.com>
2019-01-31 16:00:58 -0700

    Physics, Particles, Event (Egg)

---
+++ FirstGame1610/Assets/EggEvent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class EggEvent : MonoBehaviour
+{
+	public UnityEvent Event;
+
+	private void OnCollisionEnter2D()
+	{
+		Event. Invoke();
+	}
+}
