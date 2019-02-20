3692bf6e87e32678bd87cad4428a30e2df4b580c
Kaitlyn <kand8998@gmail.com>
2019-02-20 14:03:05 -0700

    February 20th, 2019 class follow
    Events, Functions, Power ups (Basic)

---
+++ FirstGame1610/Assets/Scripts/Butterfly.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Butterfly : WingedInsect
+{
+		// Use this for initialization
+		void Start()
+		{
+			print("Flutter, Flutter!");
+			print("Flap,Flap");
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			
+		}
+	}
+
---
+++ FirstGame1610/Assets/Scripts/MonoEvents.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MonoEvents : MonoBehaviour
+{
+	public UnityEvent StartEvent, EnableEvent, MouseDownEvent, UpdateEvent;
+	void Start () 
+	{
+		StartEvent.Invoke();
+	}
+
+	private void OnEnable()
+	{
+		EnableEvent.Invoke();
+	}
+
+	private void OnMouseDown()
+	{
+		MouseDownEvent.Invoke();
+	}
+
+	void Update () 
+	{
+		UpdateEvent.Invoke();
+	}
+}
