4d2713e6faa26a0a83cd060700c9ec7d46a070a3
Kaitlyn <kand8998@gmail.com>
2019-02-26 17:01:13 -0700

    Light Switch Tutorial Script Change

--- FirstGame1610/Assets/Scripts/SwitchTutorial.cs
+++ FirstGame1610/Assets/Scripts/SwitchTutorial.cs
@@ -5,20 +5,19 @@ using UnityEngine;
 public class SwitchTutorial : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
 	}
-	
 	// Update is called once per frame
 	private void Update () 
 	{
 		if (Input.GetKey(KeyCode.UpArrow))
 		{
-			this.GetComponent<Light>().enabled = true;
+			this.GetComponent<Light>().enabled = false;
 		}
 		else
 		{
-			this.GetComponent<Light>().enabled = false;
+			this.GetComponent<Light>().enabled = true;
 		}
 	}
 }
