3f19103fc8f7a67a7ffcd604d533c8330a0fcdcc
Kaitlyn <kand8998@gmail.com>
2019-03-05 15:24:13 -0700

    Jump force edited

--- FirstGame1610/Assets/Scripts/MoveBase.cs
+++ FirstGame1610/Assets/Scripts/MoveBase.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public abstract class MoveBase : ScriptableObject
 {
     protected Vector3 position;
-    public float JumpForce = 1.5f;
-    public float Gravity = -3;
-    public float Speed = 30;
+    public float JumpForce = 1;
+    public float Gravity = -9.8f;
+    public float Speed = 15;
     
     public abstract void Move(CharacterController controller);
 }
