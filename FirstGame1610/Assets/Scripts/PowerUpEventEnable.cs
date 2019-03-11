9e758dc28f27d547013d2c58168c56bf6e7d04cf
Kaitlyn <kand8998@gmail.com>
2019-03-10 18:15:19 -0600

    Raccoon Power Up and Base added

---
+++ FirstGame1610/Assets/Scripts/PowerUpEventEnable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PowerUpEventEnable : MonoBehaviour
+{
+
+	public UnityEvent MouseDownEvent;
+
+	private void OnMouseDown()
+	{
+		MouseDownEvent.Invoke();
+	}
+	
+
+	void Start () {
+		
+	}
+	
+	void Update()
+	{
+	}
+}
