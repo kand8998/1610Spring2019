1b0a2e93738116fa954abe8934b02ef5e6f23b80
Kaitlyn <kand8998@gmail.com>
2019-03-12 12:49:37 -0600

    Time Bar and power up bar added

--- FirstGame1610/Assets/Scripts/MonoEvents.cs
+++ FirstGame1610/Assets/Scripts/MonoEvents.cs
@@ -9,13 +9,11 @@ public class MonoEvents : MonoBehaviour
 	void Start () 
 	{
 		StartEvent.Invoke();
-		GetComponent<SpriteRenderer>();
 	}
 
 	private void OnEnable()
 	{
 		EnableEvent.Invoke();
-		transform.Rotate(0,0,0);
 	}
 
 	private void OnTriggerEnter(Collider other)
@@ -26,13 +24,10 @@ public class MonoEvents : MonoBehaviour
 	private void OnMouseDown()
 	{
 		MouseDownEvent.Invoke();
-
 	}
 
 	void Update () 
 	{
 		UpdateEvent.Invoke();
-		transform.Translate(0,0,0);
-
 	}
 }
