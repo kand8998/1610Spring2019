d682d478fcb96e4f89568c3d4cf7de801496efb8
Kaitlyn <kand8998@gmail.com>
2019-03-14 12:51:32 -0600

    PowerUp update attempt

---
+++ FirstGame1610/Assets/Scripts/PowerUpBarEvent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PowerUpBarEvent : MonoBehaviour
+{
+
+	public UnityEvent UpdateEvent;
+	private void Start () 
+	{
+		
+	}
+	
+	private void Update () 
+	{
+	
+	}
+
+	private void OnMouseDown()
+	{
+		UpdateEvent.Invoke();
+	}
+}
---
+++ FirstGame1610/Assets/Scripts/UpdatePowerUpBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdatePowerUpBar : MonoBehaviour
+{
+
+	private Image BarImage;
+	public PowerUpBase FillNumber;
+	
+	void Start ()
+	{
+		BarImage = GetComponent<Image>();
+	}
+	
+	void Update ()
+	{
+		BarImage.fillAmount = FillNumber.Value;
+	}
+}
