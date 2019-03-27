a5948f9b3fe301d27f6c4dfc25ed82e08bf95d82
Kaitlyn <kand8998@gmail.com>
2019-03-27 14:04:37 -0600

    Loops and Object Update

--- FirstGame1610/Assets/Scripts/Collection.cs
+++ FirstGame1610/Assets/Scripts/Collection.cs
@@ -6,10 +6,63 @@ using UnityEngine;
 public class Collection : ScriptableObject
 {
 
-    public List<Object> CollectionList;
+    public List<FloatData> CollectionList;
         
-    public void Collect(Object obj)
+    public void Collect(FloatData obj)
     {
         CollectionList.Add(obj);
+    }
+
+    public void CollectionInfo()
+    {
+        foreach (var obj in CollectionList)
+        {
+            Debug.Log(obj);
+        }
+    }
+
+    public void AmmoInfo()
+    {
+        foreach (var obj in CollectionList)
+        {
+            if (obj.name == "Ammo")
+            {
+                Debug.Log("We have" + obj.Value + "Ammo");
+            }
+        }
+    }
+
+    public void PlayerScoreInfo()
+    {
+        foreach (var obj in CollectionList)
+        {
+            if (obj.name == "PlayerScore")
+            {
+                Debug.Log("We have" + obj.Value + "Points!");
+            }
+        }
+    }
+
+    public void HealthInfo()
+    {
+        for (int i = 0; i < CollectionList.Count; i++)
+        {
+
+            if (CollectionList[i].name == "Health")
+            {
+                Debug.Log(CollectionList[i]);
+            }
+        }
+    }
+
+    public void GameTimeInfo()
+    {
+        for (int i = 0; i < CollectionList.Count; i++)
+        {
+            if (CollectionList[i].name == "GameTime")
+            {
+                Debug.Log(CollectionList[i]);
+            }
+        }
     }
 }
