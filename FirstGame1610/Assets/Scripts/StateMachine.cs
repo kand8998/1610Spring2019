39b0b01fb358f3c3cde68ae0470203d0b1bf7b83
Kaitlyn <kand8998@gmail.com>
2019-04-01 11:48:11 -0600

    Switch Statements Class Follow

---
+++ FirstGame1610/Assets/Scripts/StateMachine.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class StateMachine : ScriptableObject
+{
+	public UnityEvent StartingEvent, PlayingEvent, EndingEvent;
+	public enum States
+	{
+		Starting,
+		Playing,
+		Ending
+	}
+
+	public States CurrentStates;
+
+	public void OnSwitch() 
+	{
+		switch (currentStates)
+		{
+			case States.Starting:
+				StartingEvent.Invoke();
+				break;
+			case States.Playing:
+				PlayingEvent.Invoke();
+				break;
+			case States.Ending:
+				EndingEvent.Invoke();
+				break;
+		}
+	}
+}
---
+++ FirstGame1610/Assets/Scripts/Switches.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switches : MonoBehaviour 
+{
+	public enum States
+	{
+		Starting,
+		Playing,
+		Ending
+	}
+
+	public States CurrentState;
+	public UnityEvent StartingEvent, PlayingEvent, EndingEvent;
+	
+	void Update () 
+	{
+		switch (CurrentState)
+		{
+			case States.Starting:
+				StartingEvent.Invoke();
+				break;
+			case States.Playing:
+				PlayingEvent.Invoke();
+				break;
+			case States.Ending:
+				EndingEvent.Invoke();
+				break;
+		}	
+	}
+}
