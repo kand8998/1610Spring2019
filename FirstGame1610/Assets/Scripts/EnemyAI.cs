60f41b9e37e4fd60f956dc3090dbada0311717e0
Kaitlyn <kand8998@gmail.com>
2019-04-08 14:51:27 -0600

    AI

---
+++ FirstGame1610/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+	public Transform Player;
+	private NavMeshAgent agent;
+	
+	void Start ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	private void Update()
+	{
+		agent.destination = Player.position;
+	}
+}
