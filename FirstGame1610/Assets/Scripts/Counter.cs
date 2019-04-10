b39c3f6e46da732a5de423cc601e88e04f803379
Kaitlyn <kand8998@gmail.com>
2019-04-10 14:27:56 -0600

    Counter

---
+++ FirstGame1610/Assets/Scripts/Counter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Counter : MonoBehaviour
+{
+	public UnityEvent OnCountEvent, OnEndEvent;
+	public int Value = 3, MinValue = 0;
+	public float WaitTime = 1;
+
+	public void StartCounter()
+	{
+		StartCoroutine(RunCounter());
+	}
+
+	private IEnumerator RunCounter () 
+	{
+		var waitObject = new WaitForSeconds(WaitTime);
+		while (Value > MinValue)
+		{
+			yield return waitObject;
+			OnCountEvent.Invoke();
+			Value--;
+		}
+		OnEndEvent.Invoke();
+	}
+	
+
+}
