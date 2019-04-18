88dcf428bd245829d041ba7a6be701058ee10171
Kaitlyn <kand8998@gmail.com>
2019-04-18 15:45:58 -0600

    Animation Script

---
+++ FirstGame1610/Assets/Scripts/RunAnims.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunAnims : MonoBehaviour
+{
+	private Animator anims;
+	public float Speed;
+	private void Start ()
+	{
+		anims = GetComponent<Animator>();
+	}
+	
+	private void Update ()
+	{
+		anims.SetFloat("Speed", Speed);
+	}
+}
