a0f3d2bad5192d9cacd0142d2dc12d16589cd9b0
Kaitlyn <kand8998@gmail.com>
2019-05-16 08:31:04 -0600

    Conditional Score Update

--- FirstGame1610/Assets/Scripts/Arrows.cs
+++ FirstGame1610/Assets/Scripts/Arrows.cs
@@ -1,23 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
 
-public class Arrows : MonoBehaviour 
+public class Arrows : MonoBehaviour
 {
-
+	public FloatData score;
+	public UnityEvent ScoreEvent;
 	private void Start () 
-	{
-		
+	{	
 	}
 	
-
 	private void Update () 
 	{
-		
-		{
-		}
 	}
 
-	private void operator
-	
+	private void OnTriggerStay(Collider other)
+	{
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			ScoreEvent.Invoke();
+		}
+	}
 }
