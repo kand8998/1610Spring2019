97e4478876958a550a2cf26888c85884b178914c
Kaitlyn <kand8998@gmail.com>
2019-06-19 12:16:34 -0600

    appearance update
    audio, particles, script change, and background

---
+++ Practice/Assets/Scripts/MonoEvents.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MonoEvents: MonoBehaviour
+{
+
+	public UnityEvent StartEvent, UpdateEvent, TriggerEnterEvent;
+	private void Start ()
+	{
+		StartEvent.Invoke();
+	}
+	
+	private void Update ()
+	{
+		UpdateEvent.Invoke();
+	}
+
+	private void OnTriggerEnter()
+	{
+		TriggerEnterEvent.Invoke();
+	}
+}
