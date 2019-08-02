4fa79f3441b72a1dc678492a5948c328ef5db5d0
Kaitlyn <kand8998@gmail.com>
2019-08-02 16:22:51 -0600

    Update

---
+++ Practice/Assets/Scripts/Sprites.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprites : MonoBehaviour {
+
+	
+	private void Start () 
+	{
+		
+	}
+	
+	
+	private void Update ()
+	{
+		
+	}
+
+	private void OnMouseDown()
+	{
+		
+	}
+		
+}
