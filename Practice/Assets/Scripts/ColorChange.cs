5fc92497347343919d0f4cd04011dd16cdfeb076
Kaitlyn <kand8998@gmail.com>
2019-08-23 12:18:46 -0600

    3D script Material Color Change
    Script dealing with availability of input keys;

---
+++ Practice/Assets/Scripts/ColorChange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour 
+{
+
+	// Unity Color Change Tutorial
+	private void Start () 
+	{
+		
+	}
+	
+	
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.C))
+		{
+			GetComponent<Renderer>().material.color = Color.cyan;
+		}
+
+		if (Input.GetKeyUp(KeyCode.Space))
+		{
+			GetComponent<Renderer>().material.color = Color.clear;
+		}
+		
+	}
+}
