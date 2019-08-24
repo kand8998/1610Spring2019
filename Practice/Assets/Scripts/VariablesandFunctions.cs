e559c19718509256da3cdc8220ac14b6c05eef93
Kaitlyn <kand8998@gmail.com>
2019-08-24 09:08:23 -0600

    Variables and Functions
    Unity Tutorial (testing a variable with a return function besides void)

---
+++ Practice/Assets/Scripts/VariablesandFunctions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class VariablesandFunctions : MonoBehaviour 
+{
+
+	//Unity Tutorial Variables and Functions
+
+	private int PlayerScore = 5;
+	private void Start ()
+	{
+		PlayerScore = MultiplyByTwo(PlayerScore);
+		Debug.Log(PlayerScore);
+	}
+
+	protected virtual int MultiplyByTwo(int number)
+	{
+		var result = number * 5;
+		return result;
+	}
+}
